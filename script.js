6af8bcce72a39d4e499f7c3a37089341aa88a51c
Luigi Finetti <luigi@finetti.com.ar>
2021-09-19 14:13:36 -0300

    startHourglass()

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-const TIME = 10;
-var rem = TIME;
+var time = 10;
+var rem = time;
 var audio = new Audio('alarm.mp3');
-
-var x = setInterval(function() {
-  rem -=0.125;
-
-  document.getElementById("m").innerHTML = Math.floor(rem/60) + "m ";
-  document.getElementById("s").innerHTML = rem%60 + "s ";
-  document.getElementById("sandInner").setAttribute("y",String(-110+110*(rem/TIME)));
-  
-  if (rem == 6.375) {
-    audio.play();
-  }
-  
-  if (rem < 0) {
-    clearInterval(x);
-    document.getElementById("timer").innerHTML = "Terminó";
-  }
-}, 125);
+var updater;
 
 function turnHourglass() {
-  rem = TIME-rem;
+  rem = time-rem;
+};
+
+function startHourglass() {
+  updater = setInterval(function() {
+    rem -=0.125;
+  
+    document.getElementById("m").innerHTML = Math.floor(rem/60) + "m ";
+    document.getElementById("s").innerHTML = rem%60 + "s ";
+    document.getElementById("sandInner").setAttribute("y",String(-110+110*(rem/time)));
+    
+    if (rem == 6.375) {
+      audio.play();
+    }
+    
+    if (rem < 0) {
+      clearInterval(updater);
+      
+    }
+  }, 125);
 };
